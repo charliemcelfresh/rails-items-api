ac6809d566dbc448c51c9f7471c9c62c91febdf1
Charlie McElfresh <cwmcelfresh@gmail.com>
2024-04-06 15:19:05 -0700

    Change url params to fit our other test apps

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,13 +4,14 @@ class ItemsController < ApplicationController
   before_action :ensure_valid_user
 
   def index
-    page = params[:page] || 0
+    limit = params[:limit] || 10
+    offset = params[:offset] || 0
     items = current_user
       .user_items
       .joins(:item)
       .select('items.id', 'items.name', 'items.created_at', 'items.updated_at')
-      .limit(10)
-      .offset(page)
+      .limit(limit)
+      .offset(offset)
     render json: items
   end
 end
